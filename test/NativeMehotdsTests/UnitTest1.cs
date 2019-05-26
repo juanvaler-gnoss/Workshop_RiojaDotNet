4a9fdae29ca87cd610f548d41c2e4ba5570e5650
JorTurFer <jorge_turrado@hotmail.es>
2019-05-26 02:46:44 +0200

    should pass all CI

--- test/NativeMehotdsTests/UnitTest1.cs
+++ test/NativeMehotdsTests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace NativeMehotdsTests
         [InlineData(2, 1, 1)]
         [InlineData(27, 13, 14)]
         [InlineData(5, 2, 3)]
-        [InlineData(21, 1, 19)]
+        [InlineData(20, 1, 19)]
         public void Suma(int expected, int operando1, int operando2)
         {
             //Arrange
